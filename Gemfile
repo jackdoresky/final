a46a99424ea3b6162f21564ea6b8e1382e87a19f
jackdoresky <jdoresky@andrew.cmu.edu>
2017-10-13 18:12:29 -0400

    deploy

--- Gemfile
+++ Gemfile
@@ -2,9 +2,6 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'sinatra-contrib'
-gem 'dotenv-rails', :groups => [:development, :test]
-gem 'twilio-ruby', ">5.2.2"
+#gem 'dotenv-rails', :groups => [:development, :test]
 gem 'dotenv'
-gem 'password_reset'
 gem 'password_strength'
-gem 'password_characters'
--- app.rb
+++ app.rb
@@ -1,16 +1,20 @@
 require "sinatra"
 require 'sinatra/reloader' if development?
-require 'dotenv-rails'
-require 'twilio-ruby'
-rake db:migrate
-require_relative ./models/<some_model>
-enable :sessions
+#require 'dotenv-rails'
 
-@client = Twilio::REST::Client.new ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"]
+enable :sessions
 
 configure :development do
   require 'dotenv'
   Dotenv.load
+end
+
+
+
+get "/analyze" do
+  url = params[:url]
+  puts url
+  get_password_hint url
 end
 
 post "/analyze" do
@@ -19,75 +23,77 @@ post "/analyze" do
   url
 end
 
-    #Amazon
-    if body.include?("https://www.amazon.com/ap/register?openid.pape.max_auth_age=0&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&pageId=usflex&ignoreAuthState=1&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fyour-account%3Fref_%3Dnav_ya_signin&prevRID=5BGWN0VQ8AK78GMRSP77&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&prepopulatedLoginId=&failedSignInCount=0&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0")
-        message = "At least six characters"
+def get_password_hint url
 
-      #gmail
-    elsif body.include?("https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin")
-        message = "Use at least 8 characters"
+  #Amazon
+  if url.include?("https://www.amazon.com/ap/register?openid.pape.max_auth_age=0&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&pageId=usflex&ignoreAuthState=1&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fyour-account%3Fref_%3Dnav_ya_signin&prevRID=5BGWN0VQ8AK78GMRSP77&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&prepopulatedLoginId=&failedSignInCount=0&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0")
+      message = "At least six characters"
 
-      #netflix
-    elsif body.include?("https://www.netflix.com/login")
-      message = "Password should be between 4 and 50 characters"
-
-      #Spotify
-    elsif body.include?("https://www.spotify.com/us/signup/?ab=mbs")
+    #gmail
+  elsif url.include?("https://accounts.google.com/signin")
       message = "Use at least 8 characters"
 
-      #facebook
-    elsif body.include?("https://www.facebook.com/login.php?login_attempt=1&lwv=110")
-      message = "It should be longer than 6 characters, unique to you, and difficult for others to guess"
+    #netflix
+  elsif url.include?("https://www.netflix.com/login")
+    message = "Password should be between 4 and 50 characters"
 
-      #Pandora
-    elsif body.include?("https://www.pandora.com/account/sign-in")
-      message = "Your password must be at least 6 characters long"
+    #Spotify
+  elsif url.include?("https://www.spotify.com/us/signup/?ab=mbs")
+    message = "Use at least 8 characters"
 
-      #Starbucks
-    elsif body.include?("https://www.starbucks.com/account/signin")
-      message = "•	From 8 to 25 characters
+    #facebook
+  elsif url.include?("https://www.facebook.com/login.php")
+    message = "It should be longer than 6 characters, unique to you, and difficult for others to guess"
+
+    #Pandora
+  elsif url.include?("https://www.pandora.com/account/sign-in")
+    message = "Your password must be at least 6 characters long"
+
+    #Starbucks
+  elsif url.include?("https://www.starbucks.com/account/signin")
+    message = "•	From 8 to 25 characters
 •	At least one number
 •	At least one capital letter
 •	At least one lowercase letter
 •	At least one special character such as exclamation point or comma
 "
 
-      #Linkedin
-    elsif body.include?("https://www.linkedin.com/m/login/")
-      message = "6 or more characters"
+    #Linkedin
+  elsif url.include?("https://www.linkedin.com/m/login/")
+    message = "6 or more characters"
 
-      #Chegg
-    elsif body.include?("https://www.chegg.com/auth?action=login&redirect=https%3A%2F%2Fwww.chegg.com%2F&reset_password=0")
-      message = "•	At least 6 characters
+    #Chegg
+  elsif url.include?("https://www.chegg.com/auth?action=login&redirect=https%3A%2F%2Fwww.chegg.com%2F&reset_password=0")
+    message = "•	At least 6 characters
 •	Upper/lower case letters
 •	Number or punctuation
 "
 
-      #Pinterest
-    elsif body.include?("https://www.pinterest.com/login/?referrer=home_page")
-      message = "You need 6+ characters"
+    #Pinterest
+  elsif url.include?("https://www.pinterest.com/login/?referrer=home_page")
+    message = "You need 6+ characters"
 
-      #itunes
-    elsif body.include?("https://secure1.store.apple.com/shop/sign_in?c=aHR0cHM6Ly93d3cuYXBwbGUuY29tL3wxYW9zZTQyMmM4Y2NkMTc4NWJhN2U2ZDI2NWFmYWU3NWI4YTJhZGIyYzAwZQ&r=SCDHYHP7CY4H9XK2H&s=aHR0cHM6Ly93d3cuYXBwbGUuY29tL3wxYW9zZTQyMmM4Y2NkMTc4NWJhN2U2ZDI2NWFmYWU3NWI4YTJhZGIyYzAwZQ")
-      message ="Your password must have:
- 8 or more characters
+    #itunes
+  elsif url.include?("https://secure1.store.apple.com/shop/sign_in?c=aHR0cHM6Ly93d3cuYXBwbGUuY29tL3wxYW9zZTQyMmM4Y2NkMTc4NWJhN2U2ZDI2NWFmYWU3NWI4YTJhZGIyYzAwZQ&r=SCDHYHP7CY4H9XK2H&s=aHR0cHM6Ly93d3cuYXBwbGUuY29tL3wxYW9zZTQyMmM4Y2NkMTc4NWJhN2U2ZDI2NWFmYWU3NWI4YTJhZGIyYzAwZQ")
+    message ="Your password must have:
+8 or more characters
 Upper and lowercase letters
 At least one number
 "
 
-      #Venmo
-    elsif body.include?("https://venmo.com/account/sign-in")
-      message = "Must be 8 to 32 characters"
+    #Venmo
+  elsif url.include?("https://venmo.com/account/sign-in")
+    message = "Must be 8 to 32 characters"
 
-      #Paypal
-    elsif body.include?("https://www.paypal.com/signin?country.x=US&locale.x=en_US")
-      message = "8 characters or longer.
+    #Paypal
+  elsif url.include?("https://www.paypal.com/signin?country.x=US&locale.x=en_US")
+    message = "8 characters or longer.
 At least one number or symbol (like !@#$%^)
 "
 
-      #Papa Johns
-    elsif body.include?("https://www.papajohns.com/")
-      message = "•	At least 1 upper case letter
+    #Papa Johns
+  elsif url.include?("https://www.papajohns.com/")
+    message = "•	At least 1 upper case letter
 •	At least 1 lower case letter
 •	At least 1 number
 •	Be between 8 and 25 characters
@@ -95,33 +101,36 @@ At least one number or symbol (like !@#$%^)
 •	Does not have 3 of the same character in a row (Recommended)
 "
 
-    else
-      message = "Website not yet logged. Please log site."
-    end
-
+  else
+    message = "Website not yet logged. Please log site."
   end
+
+  return message
+
+end
+
 
 #Need button on interface for [Change Password] and [Password Requirements] and [Display Character/Length]
 
-if #change password is requested
-  #if user input = what it should then move to next hint
-  [#After getting all questions right will ask to provide associated email
-  #Then will route to reset password]
-
-  #if user input != hint then lockout of extension
-
-
-if #choose password requirements
-  [#post requirements]
-
-  twiml = Twilio::TwiML::MessagingResponse.new do |r|
-    r.message do |m|
-      m.body( message )
-      unless media.nil?
-        m.media( media )
-      end
-    end
-  end
-  content_type 'text/xml'
-  twiml.to_s
-end
+# if #change password is requested
+#   #if user input = what it should then move to next hint
+#   [#After getting all questions right will ask to provide associated email
+#   #Then will route to reset password]
+#
+#   #if user input != hint then lockout of extension
+#
+#
+# if #choose password requirements
+#   [#post requirements]
+#
+#   twiml = Twilio::TwiML::MessagingResponse.new do |r|
+#     r.message do |m|
+#       m.body( message )
+#       unless media.nil?
+#         m.media( media )
+#       end
+#     end
+#   end
+#   content_type 'text/xml'
+#   twiml.to_s
+# end
