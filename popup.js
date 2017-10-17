38ce52ad7aa4fc9c7adae990cc29cb89988666b1
jackdoresky <jdoresky@andrew.cmu.edu>
2017-10-16 23:10:31 -0400

    ya boi

--- popup.js
+++ popup.js
@@ -5,25 +5,19 @@ document.addEventListener('DOMContentLoaded', function() {
     chrome.tabs.getSelected(null, function(tab) {
       d = document;
 
-      //var f = d.createElement('form');
-      //f.action = 'https://tipoffapp.herokuapp.com/analyze';
-      //f.action = 'http://gtmetrix.com/analyze.html?bm';
-      //f.method = 'post';
-      //var i = d.createElement('input');
-      //i.type = 'hidden';
-      //i.name = 'url';
-      //i.value = tab.url;
-      //f.appendChild(i);
-      //d.body.appendChild(f);
-      //f.submit();
-      //alert( "Checking url" + tab.url );
-
       var xhr = new XMLHttpRequest();
       xhr.open("GET", "https://tipoffapp.herokuapp.com/analyze?url=" + tab.url, true);
       xhr.onreadystatechange = function(){
         if( xhr.readyState == 4 ){
           document.getElementById("passwordHelp").innerHTML = xhr.responseText;
-          //alert( xhr.responseText) ;
+          if( xhr.responseText.includes( "not yet logged." ) ){
+                      document.getElementById("addUrlIfUnRegistered").style.display = 'block';
+                  }
+                  else{
+                      document.getElementById("addUrlIfUnRegistered").style.display = 'none';
+
+                  }
+      }
         }
       }
       xhr.send();
