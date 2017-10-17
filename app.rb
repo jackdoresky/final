7a24fd0471d744dd595c0d2ec5253959621105f8
jackdoresky <jdoresky@andrew.cmu.edu>
2017-10-17 15:05:35 -0400

    daragh update

--- app.rb
+++ app.rb
@@ -1,6 +1,5 @@
 require "sinatra"
 require 'sinatra/reloader' if development?
-#require 'dotenv-rails'
 
 enable :sessions
 
@@ -8,8 +7,6 @@ configure :development do
   require 'dotenv'
   Dotenv.load
 end
-
-
 
 get "/analyze" do
   url = params[:url]
@@ -28,6 +25,7 @@ def get_password_hint url
   if url.nil?
     return "You must provide a URL to find out what the password requirements are."
   end
+
   #Amazon
   if url.include?("https://www.amazon.com/ap/register?openid.pape.max_auth_age=0&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&pageId=usflex&ignoreAuthState=1&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fyour-account%3Fref_%3Dnav_ya_signin&prevRID=5BGWN0VQ8AK78GMRSP77&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&prepopulatedLoginId=&failedSignInCount=0&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0")
       message = "At least six characters"
@@ -111,29 +109,3 @@ At least one number or symbol (like !@#$%^)
   return message
 
 end
-
-
-#Need button on interface for [Change Password] and [Password Requirements] and [Display Character/Length]
-
-# if #change password is requested
-#   #if user input = what it should then move to next hint
-#   [#After getting all questions right will ask to provide associated email
-#   #Then will route to reset password]
-#
-#   #if user input != hint then lockout of extension
-#
-#
-# if #choose password requirements
-#   [#post requirements]
-#
-#   twiml = Twilio::TwiML::MessagingResponse.new do |r|
-#     r.message do |m|
-#       m.body( message )
-#       unless media.nil?
-#         m.media( media )
-#       end
-#     end
-#   end
-#   content_type 'text/xml'
-#   twiml.to_s
-# end
