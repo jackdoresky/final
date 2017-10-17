db5719c5389ba223dd5377a66de983c8d38eae57
jackdoresky <jdoresky@andrew.cmu.edu>
2017-10-16 23:20:21 -0400

    last edit

--- popup.js
+++ popup.js
@@ -1,27 +1,32 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function()
+{
   var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
+  checkPageButton.addEventListener('click', function()
+  {
 
-    chrome.tabs.getSelected(null, function(tab) {
+    chrome.tabs.getSelected(null, function(tab)
+    {
       d = document;
 
       var xhr = new XMLHttpRequest();
       xhr.open("GET", "https://tipoffapp.herokuapp.com/analyze?url=" + tab.url, true);
-      xhr.onreadystatechange = function(){
-        if( xhr.readyState == 4 ){
+      xhr.onreadystatechange = function()
+      {
+        if( xhr.readyState == 4 )
+        {
           document.getElementById("passwordHelp").innerHTML = xhr.responseText;
-          if( xhr.responseText.includes( "not yet logged." ) ){
-                      document.getElementById("addUrlIfUnRegistered").style.display = 'block';
-                  }
-                  else{
-                      document.getElementById("addUrlIfUnRegistered").style.display = 'none';
-
-                  }
-      }
+          if( xhr.responseText.includes( "not yet logged." ))
+            {
+              document.getElementById("addUrlIfUnRegistered").style.display = 'block';
+            }
+          else
+            {
+              document.getElementById("addUrlIfUnRegistered").style.display = 'none';
+            }
         }
       }
+    }
       xhr.send();
 
-    });
-  }, false);
+  });
 }, false);
