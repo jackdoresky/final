fc0273ce82096884f167d6541ef286a9955ec4c3
jackdoresky <jdoresky@andrew.cmu.edu>
2017-10-17 21:19:02 -0400

    sequel

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
 require "sinatra"
 require 'sinatra/reloader' if development?
+require 'sinatra/activerecord'
+require 'sequel'
 
 enable :sessions
-
+set :database, "sqlite3:db/my_database<URL_log>.db"
 configure :development do
   require 'dotenv'
   Dotenv.load
@@ -107,6 +109,27 @@ def get_password_hint url
   else
     message = "Website not yet logged. Paste URL from above."
 
+
+DB = Sequel.sqlite # memory database, requires sqlite3
+
+DB.create_table :items do
+  primary_key :id
+  String :name
+  Float :price
+end
+
+items = DB[:items] # Create a dataset
+
+# Populate the table
+items.insert(:name => 'abc', :price => rand * 100)
+items.insert(:name => 'def', :price => rand * 100)
+items.insert(:name => 'ghi', :price => rand * 100)
+
+# Print out the number of records
+puts "Item count: #{items.count}"
+
+# Print out the average price
+puts "The average price is: #{items.avg(:price)}"
   end
 
   return message
